0f79d8bdabaa7befa3fd41bdb7d740e009758047
DreSean-Sutton <dreseansutton@gmail.com>
2022-01-31 10:14:33 -0800

    Finished javascript-primitives-and-variables assignment

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+var fullName = 'Dre Sean';
+var isCool = false;
+var totalPets = 1;
+var unknown = null;
+var never;
+
+console.log('My full name is: ', fullName);
+console.log('Am I cool?: ', isCool);
+console.log('How many pets do I have?: ', totalPets);
+console.log('the value of unknown is: ', unknown);
+console.log('the value of never is: ', never);
+
+console.log('the typeof of var fullName is: ', typeof (fullName));
+console.log('the typeof of var isCool is: ', typeof (isCool));
+console.log('the typeof of var totalPets is: ', typeof (totalPets));
+console.log('the typeof of var unknown is: ', typeof (unknown));
+console.log('the typeof of var never is: ', typeof (never));

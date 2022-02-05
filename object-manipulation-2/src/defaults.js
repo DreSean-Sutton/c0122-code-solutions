b645f3db842972ebbc2a8f5d959741a5beda0275
DreSean-Sutton <dreseansutton@gmail.com>
2022-02-04 19:24:17 -0800

    Finished object-manipulation-2 assignment

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,15 @@
 /* exported defaults */
+
+/*
+-create a for key in source loop
+  -if the key property of target is === undefined
+    assign the value of the key property of source to the key property of target
+*/
+
+function defaults(target, source) {
+  for (var key in source) {
+    if (target[key] === undefined) {
+      target[key] = source[key];
+    }
+  }
+}

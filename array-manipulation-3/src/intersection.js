765e49db7e87f433fbcb3cf947a6a136817573c3
DreSean-Sutton <dreseansutton@gmail.com>
2022-02-05 15:54:57 -0800

    Finished array-manipulation-3 assignment

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,38 @@
 /* exported intersection */
+
+/*
+-create a variable named newArr and assign it the value of an array literal
+-create a for loop that:
+a. assigns the value of 0 to i
+b. executes code block if i is less than firstArr.length
+c. i++
+  -if the includes method of secondArr with firstArr[i] as it's argument is true
+  a. and the includes method of newArr with firstArr[i] as it's argument is false:
+    call the push method of newArr with firstArr[i] as it's argument
+-create a for loop that:
+a. assigns the value of 0 to j
+b. executes code block if j is less than secondArr.length
+c. j++
+  -if the includes method of firstArr with secondArr[j] as it's argument is true
+  a. and the incldes method of newArr with secondArr[j] as it's argument is false:
+    call the push method of newArr with secondArr[j] as it's argument
+-return newArr
+*/
+
+function intersection(firstArr, secondArr) {
+  var newArr = [];
+  for (var i = 0; i < firstArr.length; i++) {
+    if ((secondArr.includes(firstArr[i])) &&
+    (!newArr.includes(firstArr[i]))) {
+      newArr.push(firstArr[i]);
+    }
+  }
+
+  for (var j = 0; j < secondArr.length; j++) {
+    if ((firstArr.includes(secondArr[j])) &&
+    (!newArr.includes(secondArr[j]))) {
+      newArr.push(secondArr[j]);
+    }
+  }
+  return newArr;
+}
--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,34 @@
 /* exported union */
+
+/*
+-create a variable named newArr and assign it the value of an empty array literal
+-create a for loop that:
+  a. assigns the value of 0 to i
+  b. executes code block if i is less than firstArr.length
+  c. i++
+  -if the includes method of newArr with firstArr[i] as it's argument is false:
+    call the push method of newArr with firstArr[i] as it's argument
+-create a for loop that:
+  a. assigns the value of 0 to i
+  b. executes code block if i is less than firstArr.length
+  c. i++
+  -if the includes method of newArr with secondArr[j] as it's argument is false
+    call the push method of newArr with secondArr[j] as it's argument
+-return newArr
+*/
+
+function union(firstArr, secondArr) {
+  var newArr = [];
+  for (var i = 0; i < firstArr.length; i++) {
+    if (!newArr.includes(firstArr[i])) {
+      newArr.push(firstArr[i]);
+    }
+  }
+
+  for (var j = 0; j < secondArr.length; j++) {
+    if (!newArr.includes(secondArr[j])) {
+      newArr.push(secondArr[j]);
+    }
+  }
+  return newArr;
+}

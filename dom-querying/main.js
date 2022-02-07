9e2c995f7fd25ab4e21ecd99b1ba731584d0bbf5
DreSean-Sutton <dreseansutton@gmail.com>
2022-02-07 10:15:24 -0800

    Finished dom-querying assignment

---
+++ dom-querying/main.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+console.log('hello, world');
+
+var $h1Element = document.querySelector('h1');
+
+var $explanation = document.querySelector('#explanation');
+
+var $hint = document.querySelector('.hint');
+
+var $allPElements = document.querySelectorAll('p');
+
+var $allExampleLinks = document.querySelector('.example-link');

3ec8f2694c1ef56612ee1f8fa7f40b8a6e07fdb9
DreSean-Sutton <dreseansutton@gmail.com>
2022-02-07 13:30:57 -0800

    Fixed swapChars algorithm

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,20 +1,34 @@
 /* exported swapChars */
 
 /*
-create a variable named newStr and assign it the value of the split method with ''
-...as it's argument of the slice method of str with 0 as it's argument
--create a variable named firstSwap and assign it the value of newStr[firstIndex]
--create a variable named secondSwap and assign it the value of newStr[secondIndex]
--call the splice method of newStr with firstIndex, 1, secondSwap as it's arguments
--call the splice method of newStr with secondIndex, 1, firstSwap as it's arguments
+-create a variable named newStr and assign it the value of an empty string literal
+-craete a variable named firstSwap and assign it the value of str at the index of the parameter firstIndex
+-craete a variable named secondSwap and assign it the value of str at the index of the parameter secondIndex
+-create a for loop that:
+a. assigns the value of 0 to i
+b. executes code block if i is less than str.length
+c. i++
+  -if i is strictly equal to firstIndex:
+    assign the value of secondSwap plus newStr to newStr
+  -else if i is strictly equal to secondIndex:
+    assign the value of firstSwap plus newStr to newStr
+  -else:
+    assign the value of str at index i plus newStr to newStr
 -return the join method of newStr with '' as it's arguments
 */
 
 function swapChars(firstIndex, secondIndex, str) {
-  var newStr = str.slice(0).split('');
-  var firstSwap = newStr[firstIndex];
-  var secondSwap = newStr[secondIndex];
-  newStr.splice(firstIndex, 1, secondSwap);
-  newStr.splice(secondIndex, 1, firstSwap);
-  return newStr.join('');
+  var newStr = '';
+  var firstSwap = str[firstIndex];
+  var secondSwap = str[secondIndex];
+  for (var i = 0; i < str.length; i++) {
+    if (i === firstIndex) {
+      newStr += secondSwap;
+    } else if (i === secondIndex) {
+      newStr += firstSwap;
+    } else {
+      newStr += str[i];
+    }
+  }
+  return newStr;
 }

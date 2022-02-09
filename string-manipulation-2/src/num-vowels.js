40d60e42219cc8630993dae645c0dce07c748a55
DreSean-Sutton <dreseansutton@gmail.com>
2022-02-09 10:39:56 -0800

    Made code more efficient and updated pseudocode to make up for it

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,29 +1,27 @@
 /* exported numVowels */
 
 /*
+-create a varialbe named lowerCaseStr and assign it the value of the
+a.toLowerCase method of the slice method of str with 0 as it's argument
 -create a variable named vowelCounter and assign it the value of 0
 -create a for loop that:
   a. assign the value of 0 to i
   b. executes code block if i is less than str.length
   c. i++
-  -if str[i] is strictly equal to 'a', A, e, E, i, I, o, O, u, U
+  -if str[i] is strictly equal to 'a', 'e', 'i', 'o', or 'u'
   increment vowelCounter by 1
 -return vowelCounter
 */
 
 function numVowels(str) {
+  var lowerCaseStr = str.slice(0).toLowerCase();
   var vowelCounter = 0;
-  for (var i = 0; i < str.length; i++) {
-    if ((str[i] === 'a') ||
-        (str[i] === 'A') ||
-        (str[i] === 'e') ||
-        (str[i] === 'E') ||
-        (str[i] === 'i') ||
-        (str[i] === 'I') ||
-        (str[i] === 'o') ||
-        (str[i] === 'O') ||
-        (str[i] === 'u') ||
-        (str[i] === 'U')) {
+  for (var i = 0; i < lowerCaseStr.length; i++) {
+    if (lowerCaseStr[i] === 'a' ||
+        lowerCaseStr[i] === 'e' ||
+        lowerCaseStr[i] === 'i' ||
+        lowerCaseStr[i] === 'o' ||
+        lowerCaseStr[i] === 'u') {
       vowelCounter++;
     }
   }

53bb1dd82e5f036a10018289865c979b0aba03b7
DreSean-Sutton <dreseansutton@gmail.com>
2022-02-11 14:43:10 -0800

    Modal will now open and close when test button is pressed. Working on having it appear automatically when a user finishes the game

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,45 +1,53 @@
-// var $allSpans = document.querySelectorAll('span');
-// var currentLetterIndex = 0;
-// var $testButton = document.querySelector('#test-button');
-// var $modal = document.querySelector('#modal');
-// var $overlay = document.querySelector('#overlay');
+var $allSpans = document.querySelectorAll('span');
+var currentLetterIndex = 0;
+var $testButton = document.querySelector('#test-button');
+var $modal = document.querySelector('#modal');
+var $overlay = document.querySelector('#overlay');
+var $noButton = document.querySelector('#no-button');
+// var $yesButton = document.querySelector('#yes-button')
 
-// document.addEventListener('keydown', typingTutor);
-// document.addEventListener('click', testingButton)
+document.addEventListener('keydown', typingTutor);
+$testButton.addEventListener('click', testingButton);
+$noButton.addEventListener('click', handleNoButton);
 
-// function testingButton (event) {
-//   if (event.target !== $testButton) {
-//     return;
-//   } else {
-//     $overlay.classList.remove('hidden');
-//     $modal.classList.remove('hidden');
-//   }
-// }
+function testingButton(event) {
+  if (event.target === $testButton) {
+    $overlay.classList.remove('hidden');
+    $modal.classList.remove('hidden');
+  }
+}
 
-// function typingTutor(event) {
-//   if (currentLetterIndex === $allSpans.length) {
-//     return;
-//   }
+function typingTutor(event) {
+  if (currentLetterIndex === $allSpans.length) {
+    return;
+  }
 
-//   $allSpans[currentLetterIndex].classList.add('border-bottom');
-//   if (event.key === $allSpans[currentLetterIndex].textContent) {
-//     $allSpans[currentLetterIndex].classList.remove('wrong-choice');
-//     $allSpans[currentLetterIndex].classList.add('correct-choice');
-//     $allSpans[currentLetterIndex].classList.remove('border-bottom');
-//     if ($allSpans[currentLetterIndex + 1] !== undefined) {
-//       $allSpans[currentLetterIndex + 1].classList.add('border-bottom');
-//     }
-//     currentLetterIndex++;
-//   } else {
-//     $allSpans[currentLetterIndex].classList.add('wrong-choice');
-//   }
-//   if ($allSpans[$allSpans.length - 1].classList === 'correct-choice border-bottom') {
-//     currentLetterIndex = 0;
-//     for (var i = 0; i < $allSpans.length; i++) {
-//       $allSpans[i].classList.remove('correct-choice');
-//       if (i > 0) {
-//         $allSpans[i].classList.remove('wrong-choice border-bottom');
-//       }
-//     }
-//   }
-// }
+  $allSpans[currentLetterIndex].classList.add('border-bottom');
+  if (event.key === $allSpans[currentLetterIndex].textContent) {
+    $allSpans[currentLetterIndex].classList.remove('wrong-choice');
+    $allSpans[currentLetterIndex].classList.add('correct-choice');
+    $allSpans[currentLetterIndex].classList.remove('border-bottom');
+    if ($allSpans[currentLetterIndex + 1] !== undefined) {
+      $allSpans[currentLetterIndex + 1].classList.add('border-bottom');
+    }
+    currentLetterIndex++;
+  } else {
+    $allSpans[currentLetterIndex].classList.add('wrong-choice');
+  }
+  if ($allSpans[$allSpans.length - 1].classList === 'correct-choice border-bottom') {
+    currentLetterIndex = 0;
+    for (var i = 0; i < $allSpans.length; i++) {
+      $allSpans[i].classList.remove('correct-choice');
+      if (i > 0) {
+        $allSpans[i].classList.remove('wrong-choice border-bottom');
+      }
+    }
+  }
+}
+
+function handleNoButton(event) {
+  if (event.target === $noButton) {
+    $modal.classList.add('hidden');
+    $overlay.classList.add('hidden');
+  }
+}

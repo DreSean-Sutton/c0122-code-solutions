25e73a4c76684809557ef30eb16b7965c8dbd66a
DreSean-Sutton <dreseansutton@gmail.com>
2022-02-11 10:38:41 -0800

    Currently making modal

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -13,12 +13,14 @@ function typingTutor(event) {
     $allSpans[currentLetterIndex].classList.remove('wrong-choice');
     $allSpans[currentLetterIndex].classList.add('correct-choice');
     $allSpans[currentLetterIndex].classList.remove('border-bottom');
-    $allSpans[currentLetterIndex + 1].classList.add('border-bottom');
+    if ($allSpans[currentLetterIndex + 1] !== undefined) {
+      $allSpans[currentLetterIndex + 1].classList.add('border-bottom');
+    }
     currentLetterIndex++;
   } else {
     $allSpans[currentLetterIndex].classList.add('wrong-choice');
   }
-  if ($allSpans[$allSpans.length - 1].classList.includes('correct-choice')) {
+  if ($allSpans[$allSpans.length - 1].classList === 'correct-choice border-bottom') {
     currentLetterIndex = 0;
     for (var i = 0; i < $allSpans.length; i++) {
       $allSpans[i].classList.remove('correct-choice');

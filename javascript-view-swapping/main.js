2f7cf51c0c7086ebe801d3d5fab44370ddb0c6d4
DreSean-Sutton <dreseansutton@gmail.com>
2022-02-10 17:38:33 -0800

    Finished javascript-view-swapping assignment

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-useless-return */
+var $allTabs = document.querySelectorAll('.tab');
+var $allViews = document.querySelectorAll('.view');
+var $tabContainer = document.querySelector('.tab-container');
+
+$tabContainer.addEventListener('click', viewSwapping);
+
+function viewSwapping(event) {
+  if (!event.target.matches('.tab')) {
+    return;
+  }
+  var targetAttribute = event.target.getAttribute('data-view');
+
+  for (var i = 0; i < $allTabs.length; i++) {
+    if (event.target === $allTabs[i]) {
+      $allTabs[i].classList.add('active');
+      $allViews[i].classList.add('active');
+    } else {
+      $allTabs[i].classList.remove('active');
+      $allViews[i].classList.remove('active');
+    }
+  }
+  for (var j = 0; j < $allViews.length; j++) {
+    if (targetAttribute === $allViews[j].getAttribute('data-view')) {
+      $allViews[j].classList.remove('hidden');
+    } else {
+      $allViews[j].classList.add('hidden');
+    }
+  }
+}

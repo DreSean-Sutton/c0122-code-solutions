053c389f835d52009a1327d2078847f5429ce4e9
DreSean-Sutton <dreseansutton@gmail.com>
2022-02-22 11:04:27 -0800

    Finished javascript-constructor-assignment

---
+++ javascript-constructors/student.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+/* exported Student */
+
+function Student(firstName, lastName, subject) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.subject = subject;
+}
+
+Student.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+
+Student.prototype.getIntroduction = function () {
+  return `Hello, my name is ${this.firstName} ${this.lastName} and I am studying ${this.subject}.`;
+};
+
+var me = new Student('Dre Sean', 'Sutton', 'CSS');

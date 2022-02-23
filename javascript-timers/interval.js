a6b55118e3a5a75b5f096cf3a3d61a5fa05f6512
DreSean-Sutton <dreseansutton@gmail.com>
2022-02-23 10:31:49 -0800

    Finished javascript-timers assignment

---
+++ javascript-timers/interval.js
@@ -0,0 +1,10 @@
+var $h1 = document.querySelector('h1');
+
+var myInterval = setInterval(() => {
+  if ($h1.textContent === '0') {
+    $h1.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(myInterval);
+  } else {
+    $h1.textContent--;
+  }
+}, 1000);

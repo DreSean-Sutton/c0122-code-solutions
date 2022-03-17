1109695e69056b6a5e4d279474b7d5bbc883cf0d
DreSean-Sutton <dreseansutton@gmail.com>
2022-03-17 16:04:26 -0700

    Finished refactoring code for better QoL

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -13,7 +13,7 @@ const handleNotes = {
 if (process.argv[2] === 'read') {
   handleNotes.read();
 } else if (process.argv[2] === 'create') {
-  handleCreation();
+  handleNotes.create();
 } else if (process.argv[2] === 'delete') {
   handleNotes.delete();
 } else if (process.argv[2] === 'update') {
@@ -23,6 +23,10 @@ if (process.argv[2] === 'read') {
 function handleRead() {
 
   for (let i = 1; i < dataJSON.nextId; i++) {
+
+    if (dataJSON.notes[i.toString()] === undefined) {
+      continue;
+    }
     console.log(i + ': ' + dataJSON.notes[i.toString()]);
   }
 }
@@ -41,19 +45,25 @@ function handleCreation() {
 
 function handleDelete() {
 
+  if (dataJSON.notes[process.argv[3].toString()] === undefined) {
+    console.log('I\'m sorry, cannot delete notes that doesn\'t exist.');
+    return;
+  }
+
   delete dataJSON.notes[process.argv[3].toString()];
   handleWriteFile();
 }
 
 function handleUpdate() {
 
-  if (process.argv[3] >= dataJSON.nextId) {
+  if (dataJSON.notes[process.argv[3].toString()] === undefined) {
     console.log('I\'m sorry, cannot update notes that doesn\'t exist');
     return;
   }
 
   let newNote = '';
   for (let i = 4; i < process.argv.length; i++) {
+
     newNote += `${process.argv[i]} `;
   }
   newNote = `${newNote.slice(0, newNote.length - 1)}`;

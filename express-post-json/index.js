2d141ad1233901f7d073ae350a7296aa3137d2d1
DreSean-Sutton <dreseansutton@gmail.com>
2022-03-21 11:30:49 -0700

    Work in progress (I think?)

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
-let grade = {};
+const grades = {};
 let nextId = 1;
-const gradesArray = [];
 
 app.get('/api/grades', (req, res) => {
-
-  res.json(gradesArray);
+  const getArray = [];
+  console.log(grades);
+  for (const key in grades) {
+    getArray.push(grades[key]);
+    console.log(grades);
+  }
+  res.json(getArray);
 });
 
 const expressJSON = express.json();
@@ -14,11 +18,13 @@ const expressJSON = express.json();
 app.use('/api/grades', expressJSON);
 
 app.post('/api/grades', (req, res) => {
-  req.body.id = nextId;
-  grade = req.body;
-  gradesArray.push(grade);
+  const newGrade = req.body;
+  const id = nextId;
+  newGrade.id = id;
+  grades[id] = newGrade;
+  console.log(newGrade);
   nextId++;
-  res.status(201).send(grade);
+  res.status(201).json(newGrade);
 });
 
 app.listen(3000, () => {

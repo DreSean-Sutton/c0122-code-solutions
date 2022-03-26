b9634b6d13716f844144076c6c3d9cac159747a6
DreSean-Sutton <dreseansutton@gmail.com>
2022-03-25 19:37:21 -0700

    user can now search for for a single row baseed on its gradeId

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -17,13 +17,39 @@ app.get('/api/grades', (req, res) => {
     SELECT *
     FROM
       "grades"
-      ORDER BY
+    ORDER BY
         "name"
       `;
   db.query(sql)
     .then(result => {
       const allGrades = result.rows;
       res.json(allGrades);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occurred.' });
+    });
+});
+
+app.get('/api/grades/:gradeId', (req, res) => {
+  const currentId = req.params.gradeId;
+  const text = `
+    SELECT *
+    FROM
+      "grades"
+    WHERE
+      "gradeId" = $1
+    `;
+  const values = [currentId];
+  if (currentId < 0) {
+    return res.status(400).json({ error: 'id must be a positive integer.' });
+  }
+  db.query(text, values)
+    .then(result => {
+      if (result.rows.length === 0) {
+        res.status(404).json({ error: `gradeId ${currentId} doesn't exist.` });
+      }
+      res.json(result.rows[0]);
     })
     .catch(err => {
       console.error(err);
@@ -50,7 +76,7 @@ app.post('/api/grades', (req, res) => {
     res.status(400).json({ error: 'name, course, and score are required fields' });
   } else if (newGrade.score < 0 ||
     newGrade.score > 100) {
-    res.status(400).json({ error: 'score must be from 0 to 100' });
+    res.status(400).json({ error: 'score must be from 0 to 100.' });
   } else {
     db.query(text, values)
       .then(result => {
@@ -89,14 +115,14 @@ app.put('/api/grades/:gradeId', (req, res) => {
     res.status(400).json({ error: 'name, course, and score are required fields' });
   } else if (updatedGrade.score < 0 ||
     updatedGrade.score > 100) {
-    res.status(400).json({ error: 'score must be from 0 to 100' });
+    res.status(400).json({ error: 'score must be from 0 to 100.' });
   } else if (currentId < 1) {
-    res.status(400).json({ error: 'id must be greater than 0' });
+    res.status(400).json({ error: 'id must be a positive integer.' });
   } else {
     db.query(text, values)
       .then(result => {
         if (result.rows.length === 0) {
-          res.status(404).json({ error: `gradeId ${currentId} doesn't exist` });
+          res.status(404).json({ error: `gradeId ${currentId} doesn't exist.` });
         } else {
           res.json(result.rows[0]);
         }
@@ -119,12 +145,12 @@ app.delete('/api/grades/:gradeId', (req, res) => {
   `;
   const values = [currentId];
   if (currentId < 1) {
-    res.status(400).json({ error: 'id must be greater than 0' });
+    res.status(400).json({ error: 'id must be a positive integer.' });
   } else {
     db.query(text, values)
       .then(result => {
         if (result.rows.length === 0) {
-          res.status(404).json({ error: `gradeId ${currentId} doesn't exist` });
+          res.status(404).json({ error: `gradeId ${currentId} doesn't exist.` });
         } else {
           res.status(204).json(result.rows[0]);
         }

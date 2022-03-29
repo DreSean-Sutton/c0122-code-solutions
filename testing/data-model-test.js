cbaf1b0c4ef326bf586a84a31394a0537181dc46
DreSean-Sutton <dreseansutton@gmail.com>
2022-03-29 15:04:55 -0700

    Just testing data modeling

---
+++ testing/data-model-test.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+const inkling = {
+  moves: [
+    {
+      name: 'jab 1',
+      activeFrames: 3 - 4,
+      totalFrames: 19
+    },
+    {
+      name: 'jab 2',
+      activeFrames: 2 - 3,
+      totalFrames: 21
+    },
+    {
+      name: 'jab 3',
+      activeFrames: 4 - 5,
+      totalFrames: 29
+    },
+    {
+      name: 'rapid jab',
+      activeFrames: 4 '(rehit rate: 3 frames)'
+    }
+
+  ]
+};

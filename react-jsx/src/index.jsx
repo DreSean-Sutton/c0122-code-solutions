a32ddd424d23b6beac7c66828776ecf0f86b65c5
DreSean-Sutton <dreseansutton@gmail.com>
2022-03-29 13:09:53 -0700

    Added arbitrary testing

--- react-jsx/src/index.jsx
+++ react-jsx/src/index.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const element = <h1>Hello, JSX!</h1>;
-
+const anotherElement = <ul>
+  <li>No</li>
+  <li>Way</li>
+  <li>This</li>
+  <li>Works</li>
+  <li>With</li>
+  <li>Emmet</li>
+  <li>uwu</li>
+</ul>;
 ReactDOM.render(
   element,
   document.querySelector('#root')
+);
+
+ReactDOM.render(
+  anotherElement,
+  document.querySelector('#sibling-root')
 );

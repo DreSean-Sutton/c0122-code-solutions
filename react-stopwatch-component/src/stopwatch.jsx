8c4975540dfbb593898ed13c2c117a25e8af0091
DreSean-Sutton <dreseansutton@gmail.com>
2022-03-31 16:55:38 -0700

    Completed react-stopwatch-component

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -3,43 +3,80 @@ import React from 'react';
 class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+    this.mountComponent = this.mountComponent.bind(this);
+    this.handleTick = this.handleTick.bind(this);
+    this.dismountComponent = this.dismountComponent.bind(this);
     this.state = {
-      seconds: 0,
-      isTicking: false
+      intervalId: null,
+      isReset: true,
+      isTicking: false,
+      seconds: 0
     };
-  //   const intervalId = setInterval(() => {
-
-  //   });
-  // }
-  componentDidMount() {
-    this.intervalId = setInterval(() => {
-      this.setState({
-        isTicking: true
-      })
-    })
   }
-  handleClick() {
-    this.setState = ({
-      seconds: this.state + 1
+
+  mountComponent() {
+    if (!this.state.isTicking) {
+      this.setState({
+        isTicking: !this.state.isTicking
+      });
+      this.handleTick();
+    }
+    if (this.state.isTicking) {
+      this.setState({
+        isTicking: !this.state.isTicking
+      });
+      this.stopTime();
+    }
+  }
+
+  handleTick() {
+    this.setState({
+      isReset: false,
+      intervalId: setInterval(() => {
+        this.setState({
+          seconds: this.state.seconds + 1
+        });
+      }, 1000)
     });
   }
 
-  helperClasses() {
-    if (this.state.isTicking) {
-      return 'play';
-    } else {
-      return 'pause';
+  stopTime() {
+    clearInterval(this.state.intervalId);
+    this.setState({
+      isTicking: false,
+      intervalId: null
+    });
+  }
+
+  dismountComponent() {
+    if (!this.state.isTicking) {
+      this.setState({
+        seconds: 0,
+        isReset: true
+      });
     }
-    clearInterval(this.intervalId);
+  }
+
+  coloring() {
+    if (this.state.isReset) return 'stop';
+    if (this.state.isTicking) return 'play';
+    if (!this.state.iTicking) return 'pause';
+  }
+
+  helperClasses() {
+    if (this.state.isTicking) return 'pause';
+    if (!this.state.iTicking) return 'play';
   }
 
   render() {
     return (
     <div className="container">
       <div className="stopwatch-layout">
-        <div className="stopwatch-body">
-        <span>{ this.state.seconds }</span>
+          <div onClick={this.dismountComponent} className={`stopwatch-body ${this.coloring()}-border`}>
+            <span className={`${this.coloring()}-color`}>{ this.state.seconds }</span>
+        </div>
+        <div>
+            <i onClick={ this.mountComponent } className={`fa-solid fa-circle-${this.helperClasses()} play-pause-buttons`}></i>
         </div>
       </div>
     </div>
@@ -47,6 +84,5 @@ class Stopwatch extends React.Component {
   }
 }
 
-/* <FontAwesomeIcon icon="fa-solid fa-circle-pause" /> */
-/* <FontAwesomeIcon icon="fa-solid fa-circle-play" /> */
+/* <FontAwesomeIcon icon="fa-solid fa-circle-pause" />; */
 export default Stopwatch;

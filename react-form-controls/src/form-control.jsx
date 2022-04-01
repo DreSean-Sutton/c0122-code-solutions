620aa02560e634ae9b6d252709d9235aaf814eea
DreSean-Sutton <dreseansutton@gmail.com>
2022-04-01 12:50:17 -0700

    Completed react-form-controls

---
+++ react-form-controls/src/form-control.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      placeholder: true,
+      emailValue: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      emailValue: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.emailValue);
+  }
+
+  render() {
+    return (
+      <form onSubmit={ this.handleSubmit }>
+        <label htmlFor="email">email
+          <input onChange={ this.handleChange } id="email" type="email" />
+        </label>
+        <input type="submit" value="Sign Up"/>
+      </form>
+    );
+  }
+}

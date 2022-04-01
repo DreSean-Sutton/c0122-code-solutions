a4093b60e1103c6b1210f0e50110cdc3a1759d24
DreSean-Sutton <dreseansutton@gmail.com>
2022-04-01 02:51:55 -0700

    I'll make the transition go both ways later, I need sleep

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -6,31 +6,47 @@ class AppDrawer extends React.Component {
     this.state = {
       drawerOpen: false
     };
-    this.handleClick = this.handleClick.bind(this);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.handleHidden = this.handleHidden.bind(this);
+    this.handleTransition = this.handleTransition.bind(this);
   }
 
   toggleNavbar() {
-
+    this.setState({
+      drawerOpen: !this.state.drawerOpen
+    });
   }
 
-  handleClick() {
+  handleHidden(boolean) {
+    return boolean ? 'hidden' : '';
+  }
+
+  handleTransition() {
+    return this.state.drawerOpen
+      ? 'navbar-in'
+      : 'navbar-out';
+  }
+
+  handleOverlay() {
+    return this.state.drawerOpen
+      ? 'overlay-on'
+      : 'overlay-off';
   }
 
   render() {
-    const overlay = this.state.drawerOpen
-      ? ''
-      : '';
+    const toggleOverlay = this.handleHidden(!this.state.drawerOpen);
+    const overlayAnimation = this.handleOverlay();
+    const toggleIcon = this.handleHidden(this.state.drawerOpen);
     return (
       <div className="navbar-container">
-        <div className= {`overlay ${overlay}`} />
-        <div className="wrapper">
-        <div className="row align-items-center justify-between">
+            <i onClick={this.toggleNavbar} className={`fa-solid fa-bars navbar ${toggleIcon}`}></i>
+        <div onClick={this.toggleNavbar} className= {`overlay ${overlayAnimation} ${toggleOverlay}`}>
+          <div className={`navbar-layout ${this.handleTransition()}`}>
             <h1>menu</h1>
-            <i className="fa-solid fa-bars navbar"></i>
-        </div>
-        <a href="#">about</a>
-        <a href="#">get started</a>
-        <a href="#">sign in</a>
+            <a href="#">about</a>
+            <a href="#">get started</a>
+            <a href="#">sign in</a>
+          </div>
         </div>
       </div>
     );

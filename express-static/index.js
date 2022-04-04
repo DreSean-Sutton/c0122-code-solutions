c25ab5726e2755ca03670b50a9f6bd6fe6a459d5
DreSean-Sutton <dreseansutton@gmail.com>
2022-04-04 16:09:38 -0700

    Slightly modified index.js

--- express-static/index.js
+++ express-static/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-const curiousValue = path.join(__dirname, 'public');
-const publicRoot = express.static(curiousValue);
+const publicPath = path.join(__dirname, 'public');
+const staticMiddleware = express.static(publicPath);
 
-app.use('/', publicRoot);
+app.use(staticMiddleware);
 
 app.listen(port, () => console.log(`listening on port ${port}`));
